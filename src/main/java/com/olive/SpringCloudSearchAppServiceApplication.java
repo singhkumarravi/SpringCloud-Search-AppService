12b973c2289ed4ec6c6fe54f7baa3512488c254b
Ravi <Ravi@DESKTOP-5A7HVVO>
2021-09-10 19:57:17 +0530

    Search-AppService-1

---
+++ src/main/java/com/olive/SpringCloudSearchAppServiceApplication.java
@@ -0,0 +1,15 @@
+package com.olive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringCloudSearchAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudSearchAppServiceApplication.class, args);
+	}
+
+}
